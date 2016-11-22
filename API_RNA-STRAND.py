d10763d7aabf379ddd7b908ee4ade4732fdbd167
joannazbijewska <asia.zbijewska@gmail.com>
2016-11-22 23:01:35 +0100

    Ściąganie po ID
    Narazie ściągają się metadane w postaci jednej listy.

--- API_RNA-STRAND.py
+++ API_RNA-STRAND.py
@@ -4,6 +4,7 @@
 __author__ = "Asia Zbijewska, Gruszka"
 """
 
+import re
 import csv
 import urllib
 import requests
@@ -23,11 +24,42 @@ class RNA_STRAND():
 
     def fetch_by_sequence(self):
         urlrna = "http://www.rnasoft.ca/strand/"
-        search = urlrna+"search_results.php?select%5B%5D=Any+type&org=&source%5B%5D=Any+source&source_id=&select2=Any+length&first1=&last1=&exp_proven=Any&select4=Any+number&first2=&last2=&select5=Any&select_duplicate=All+molecules&seq={}&abstractshapetype=ABSTRACT_SHAPE_5&abstractshape=&Submit=Perform+search&select6=Any+number&first3=&last3=&select7=Any+number&first4=&last4=&select8=Any+number&first5=&last5=&select12=Any+number&first7=&last7=&select13=Any+number&first8=&last8=&motif=&select19=Any+number&first10=&last10=&select20=Any+number&first11=&last11=&select25=Any+number&first13=&last13=&select26=Any+number&first14=&last14=&select27=absolute&select28=Any+number&first15=&last15=&select33=Any+number&first17=&last17=&select34=Any+number&first18=&last18=&select35=average&select36=absolute&select37=Any+number&first19=&last19=&select38=Any+number&first20=&last20=&select69=Any+number&first31=&last31=&select71=bands&select72=Any+number&first32=&last32=&select73=base+pairs&select74=Any+number&first33=&last33=&select75=Any&select76=Any+number&first34=&last34=&select55=any&select_ncbp_context_2=any&select_ncbp_context_4=any&select_ncbp_context_1=any&select_ncbp_context_5=any&select56=Any+number&first26=&last26=&start=0&limit=10".format(self.sequence)
+        search = urlrna+"search_results.php?select%5B%5D=Any+type&org=&source%5B%5D=Any+source&source_id=&select2=Any+length&first1=&last1=&exp_proven=Any&select4=Any+number&first2=&last2=&select5=Any&select_duplicate=All+molecules&seq={}&abstractshapetype=ABSTRACT_SHAPE_5&abstractshape=&Submit=Perform+search&select6=Any+number&first3=&last3=&select7=Any+number&first4=&last4=&select8=Any+number&first5=&last5=&select12=Any+number&first7=&last7=&select13=Any+number&first8=&last8=&motif=&select19=Any+number&first10=&last10=&select20=Any+number&first11=&last11=&select25=Any+number&first13=&last13=&select26=Any+number&first14=&last14=&select27=absolute&select28=Any+number&first15=&last15=&select33=Any+number&first17=&last17=&select34=Any+number&first18=&last18=&select35=average&select36=absolute&select37=Any+number&first19=&last19=&select38=Any+number&first20=&last20=&select69=Any+number&first31=&last31=&select71=bands&select72=Any+number&first32=&last32=&select73=base+pairs&select74=Any+number&first33=&last33=&select75=Any&select76=Any+number&first34=&last34=&select55=any&select_ncbp_context_2=any&select_ncbp_context_4=any&select_ncbp_context_1=any&select_ncbp_context_5=any&select56=Any+number&first26=&last26=&start=0&limit=100&sort_by=length&order=ascending".format(self.sequence)
         found = requests.get(search)
         output = html.fromstring(found.content)
-        results = output.xpath('')
-        #Tutaj powinna sie znalezc czesc, ktora pozwoli urzytkownikowi wybrac
-        #Bo w zaleznosci od sekwencji ilosc wynikow moze byc rozna
-        #Trzeba wylistowac wyniki i dac opcje wyboru
-        #W zaleznosci od niego dalsza sciezka funkcji
+        output_list = output.xpath('//tr/td/text()')
+        regex1 = re.compile(r'(Any)')
+        regex2 = re.compile(r'(\n)')
+        regex3 = re.compile(r'(\t)')
+        regex4 = re.compile(r'(Unknown)')
+        regex5 = re.compile(r'\[')
+        regex6 = re.compile(r'(In all)')
+        regex7 = re.compile(r'\ ')
+        regex8 = re.compile(r'(Remove outliers)')
+        regex9 = re.compile(r'(Normalize by RNA type)')
+        out = [regex7,regex8,regex9]
+        result1=[]
+        for i in output_list:
+            if regex1.search(i)==None:
+                result1.append(i)
+        result2=[]
+        for i in result1:
+            if regex2.search(i)==None:
+                result2.append(i)
+        result3=[]
+        for i in result2:
+            if regex3.search(i)==None:
+                result3.append(i)
+        result4=[]
+        for i in result3:
+            if regex4.search(i)==None:
+                result4.append(i)
+        result5=[]
+        for i in result4:
+            if regex5.search(i)==None:
+                result5.append(i)
+        result6=[]
+        for i in result5:
+            if regex6.search(i)==None:
+                result6.append(i)
+        result = [x for x in result6 if not any (regex.match(x) for regex in out)]
