86691fc4dafc0d21094a78b246628831338a59f2
michal.karlicki@gmail.com <michal.karlicki@gmail.com>
2017-01-07 12:37:54 +0100

    Some updates

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 print "module prettytable is needed"
 
 import API_PROJEKT
-#import API_RNA_STRAND
+import API_RNA_STRAND
 import os
 
 def rmsd_calculation(x, y):
@@ -10,4 +10,4 @@ def rmsd_calculation(x, y):
     os.system("python calculate_rmsd "+x+" "+y)
 
 
-rmsd_calculation("5swe.pdb", "5swe.pdb")
+#rmsd_calculation("5swe.pdb", "5swe.pdb")
